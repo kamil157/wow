773d6ba3c91d9aa66112785d733e5b97520950ee
Kamil Chlodnicki <kamil.chlodnicki@mobica.com>
2017-07-14 15:59:25 +0200

    Dont use title case

--- simc/urls.py
+++ simc/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'simc'
 
 urlpatterns = [
+    # TODO slug instead of id?
     url(r'^talents/(?P<class_id>[0-9]{1,2})/(?P<spec>[0-3])$', views.get_talents, name='talents'),
     url(r'^character/$', views.CharacterView.as_view(), name='character'),
 ]
