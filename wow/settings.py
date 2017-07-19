1dd4dd63549339083669530a9924feae9337f858
Kamil Chlodnicki <kamil.chlodnicki@mobica.com>
2017-07-19 11:02:07 +0200

    Add initial heroku settings

--- wow/settings.py
+++ wow/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '_static'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 if not DEBUG:
     X_FRAME_OPTIONS = 'DENY'
