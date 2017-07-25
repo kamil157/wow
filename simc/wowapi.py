ba36bd2aad8a8ad87ea1fa82b1cba51a5a8c1056
Kamil Chlodnicki <kamil.chlodnicki@mobica.com>
2017-07-25 11:54:35 +0200

    Add wow api docstring

--- simc/wowapi.py
+++ simc/wowapi.py
@@ -16,9 +16,65 @@ def get(resource):
 
 
 def get_classes():
+    """
+    Returns:
+        dict(
+            'classes': list(
+                dict(
+                    'id': int,
+                    'mask': int,
+                    'name': str,
+                    'powerType': str
+                )
+            )
+        )
+    """
     return get('character/classes')
 
 
 def get_talents():
+    """
+    Returns:
+        dict(
+            '1': dict(
+                'talents': list(
+                    list(
+                        list(
+                            dict(
+                                'spell': dict(
+                                    'description': str,
+                                    'name': str,
+                                    'icon': str,
+                                    'id': int,
+                                    'castTime': str
+                                ),
+                                'spec': dict(
+                                    'role': str,
+                                    'description': str,
+                                    'icon': str,
+                                    'order': int,
+                                    'name': str,
+                                    'backgroundImage': str
+                                ),
+                                'tier': int,
+                                'column': int
+                            )
+                        )
+                    )
+                ),
+                'class': str,
+                'specs': list(
+                    dict(
+                        'role': str,
+                        'description': str,
+                        'icon': str,
+                        'order': int,
+                        'name': str,
+                        'backgroundImage': str
+                    )
+                )
+            )
+            '2': ...,
+    """
     # TODO would be nice if this structure was defined in code and easier to navigate
     return get('talents')
